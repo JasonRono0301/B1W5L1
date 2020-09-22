aea5ccc61720c829a66941579a61ba830700230e
Jason R <99057964@mydavinci.nl>
2020-09-22 15:16:06 +0200

    Kleine aanpassingen bij de lab opdracht.

--- assets/kaas.js
+++ assets/kaas.js
@@ -7,26 +7,32 @@ const JA = 'ja';
 const NEE = 'nee';
 
 vragen = prompt('Is de kaas geel?');
+
 if (vragen == JA) {
     alert('Dit anwtoord is goed!')
+        vragen = prompt('Zitten er gaten in?');
+            vragen = true;
 } else {
     alert('Dit antwoord is fout!');
-    
+        vragen = false;
 };
 
-vragen = prompt('Zitten er gaten in?');
 if (vragen == NEE) {
     alert('Dit antwoord is goed!');
+         vragen = prompt('Is de kaas hard als steen?');
+            vragen = true;
 } else{
     alert('Dit antwoord is fout');
+        vragen = false;
 };
 
-vragen = prompt('Is de kaas hard als steen?');
 if (vragen == NEE) {
     alert('Dit antwoord is goed!');
-    var kaasSoort = alert('De kaas soort is Pamigiano Reggiano.');
+        alert('U denk aan de kaas soort Pamigiano Reggiano.');
+            vragen = true;
 } else{
     alert('Dit antwoord is fout');
+        vragen = false;
 };
 
 
